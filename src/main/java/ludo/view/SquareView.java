783afcfc0744af4d7a85438f6e0f19a83e7eaa3c
Daniel Serezane <serezane@protonmail.com>
2023-05-05 13:52:00 -0300

    redraw method

--- src/main/java/ludo/view/SquareView.java
+++ src/main/java/ludo/view/SquareView.java
@@ -5,8 +5,10 @@ import javax.swing.*;
 import ludo.model.Color;
 
 public class SquareView extends JLabel {
+    private int pawnCount;
     public SquareView() {
         super("");
+        this.pawnCount = 0;
         // Seta tamanho do quadrado (45px)
         setPreferredSize(new java.awt.Dimension(45, 45));
     }
@@ -20,5 +22,10 @@ public class SquareView extends JLabel {
         // Adiciona peça
         //ImageIcon pawn = new ImageIcon("pawn_" + color.toString().toLowerCase() + ".png");
         //setIcon(pawn);
+        this.pawnCount++;
+    }
+
+    private void redraw() {
+        System.out.println("redraw");
     }
 }
