bc38f4bd5dac07064181561292be9e05a7d0b5ee
Salies <serezane@protonmail.com>
2023-05-07 20:09:31 -0300

    update interface conexão

--- src/main/java/ludo/view/MainWindow.java
+++ src/main/java/ludo/view/MainWindow.java
@@ -186,6 +186,8 @@ public class MainWindow extends javax.swing.JFrame {
         sb.append("Seu IP local é: ");
         sb.append(localIP);
         JOptionPane.showMessageDialog(this, sb.toString(), "Seu IP", JOptionPane.INFORMATION_MESSAGE);
+        // Disable the connect option
+        this.conectarMenuItem.setEnabled(false);
     }//GEN-LAST:event_serHostMenuItemActionPerformed
 
     private void sobreMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sobreMenuItemActionPerformed
@@ -224,6 +226,8 @@ public class MainWindow extends javax.swing.JFrame {
             historicoPane.setText("Conectado ao servidor " + address.getHostAddress() + "\n");
             // Enable dice rolling
             rollButton.setEnabled(true);
+            // Disable hosting option
+            this.serHostMenuItem.setEnabled(false);
         } catch (UnknownHostException ex) {
             JOptionPane.showMessageDialog(this, "Erro na conexão.", "Erro", JOptionPane.ERROR_MESSAGE);
         }
